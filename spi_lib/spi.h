ac621d9cdc0724e7a61f4d66e527102085fe9c76
Haluk Simsek <37951127+haluks@users.noreply.github.com>
2022-03-18 11:31:23 +0300

    Add files via upload

---
+++ spi_lib/spi.h
@@ -0,0 +1,30 @@
+/*
+ * spi.h
+ *
+ * Created: 9.01.2021 14:50:36
+ *  Author: haluk
+ */ 
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include <avr/io.h>
+//#include <xc.h>
+/////////////////////////////////
+#define SS				PORTB2
+#define MOSI			PORTB3
+#define MISO			PORTB4
+#define SCK				PORTB5
+#define SPI_PORT		PORTB
+#define SPI_DDR			DDRB
+#define SS_HIGH			SPI_PORT|=(1<<SS)
+#define SS_LOW			SPI_PORT&=~(1<<SS)
+
+void spi_basla();
+uint8_t spi_data(uint8_t sData);
+
+
+
+
+
+#endif
